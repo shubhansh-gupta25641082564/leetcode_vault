b5f2a8f644c9793b78866b46ff4449a7665bf5a7
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-11 22:03:39 +0530

    Create 2785-SortVowelsinaString

---
+++ 2785-SortVowelsinaString/2785-SortVowelsinaString.java
@@ -0,0 +1,22 @@
+// Last updated: 11/09/2025, 22:03:37
+class Solution {
+    public String sortVowels(String s) {
+        int n = s.length();
+        char[] arr = s.toCharArray();
+        java.util.List<Character> vowels = new java.util.ArrayList<>();
+        for (char c : arr) {
+            if (isVowel(c)) vowels.add(c);
+        }
+        java.util.Collections.sort(vowels);
+        int idx = 0;
+        for (int i = 0; i < n; ++i) {
+            if (isVowel(arr[i])) {
+                arr[i] = vowels.get(idx++);
+            }
+        }
+        return new String(arr);
+    }
+    private boolean isVowel(char c) {
+        return c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U';
+    }
+}
