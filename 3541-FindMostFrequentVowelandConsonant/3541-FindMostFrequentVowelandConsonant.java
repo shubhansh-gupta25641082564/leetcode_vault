8e8234251d677310b0f4eb6bced3e1704ef54d61
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-13 12:01:10 +0530

    Create 3541-FindMostFrequentVowelandConsonant

---
+++ 3541-FindMostFrequentVowelandConsonant/3541-FindMostFrequentVowelandConsonant.java
@@ -0,0 +1,28 @@
+// Last updated: 13/09/2025, 12:01:08
+class Solution {
+    public int maxFreqSum(String s) {
+        int[] freq = new int[26];
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        boolean[] isVowel = new boolean[26];
+        isVowel['a' - 'a'] = true;
+        isVowel['e' - 'a'] = true;
+        isVowel['i' - 'a'] = true;
+        isVowel['o' - 'a'] = true;
+        isVowel['u' - 'a'] = true;
+
+        int maxVowel = 0;
+        int maxConsonant = 0;
+        for (int i = 0; i < 26; i++) {
+            if (isVowel[i]) {
+                maxVowel = Math.max(maxVowel, freq[i]);
+            } else {
+                maxConsonant = Math.max(maxConsonant, freq[i]);
+            }
+        }
+
+        return maxVowel + maxConsonant;
+    }
+}
