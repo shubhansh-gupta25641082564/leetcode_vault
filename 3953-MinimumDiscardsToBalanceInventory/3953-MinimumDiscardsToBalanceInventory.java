1744eab4ade083589c9e463afd14361c7dd421b8
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-15 22:05:09 +0530

    Create 3953-MinimumDiscardsToBalanceInventory

---
+++ 3953-MinimumDiscardsToBalanceInventory/3953-MinimumDiscardsToBalanceInventory.java
@@ -0,0 +1,24 @@
+// Last updated: 15/09/2025, 22:05:07
+class Solution {
+    public int minArrivalsToDiscard(int[] arrivals, int w, int m) {
+        int n = arrivals.length;
+        int maxType = 0;
+        for (int v : arrivals) if (v > maxType) maxType = v;
+        int[] counts = new int[maxType + 1];
+        boolean[] kept = new boolean[n];
+        int discarded = 0;
+        for (int i = 0; i < n; ++i) {
+            int outIdx = i - w;
+            if (outIdx >= 0 && kept[outIdx]) counts[arrivals[outIdx]]--;
+            int t = arrivals[i];
+            if (counts[t] < m) {
+                kept[i] = true;
+                counts[t]++;
+            } else {
+                kept[i] = false;
+                discarded++;
+            }
+        }
+        return discarded;
+    }
+}
