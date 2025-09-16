feffd524ca0b58988be5d7e886c6a34feb4d423a
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-16 22:56:19 +0530

    File updated at 16/09/2025, 22:56:16

--- 78-Subsets/78-Subsets.java
+++ 78-Subsets/78-Subsets.java
@@ -1,22 +1,12 @@
-// Last updated: 16/09/2025, 22:55:03
+// Last updated: 16/09/2025, 22:56:16
 class Solution {
-    public boolean exist(char[][] b, String w) {
-        int m = b.length, n = b[0].length;
-        int L = w.length();
-        for (int i = 0; i < m; i++)
-            for (int j = 0; j < n; j++)
-                if (b[i][j] == w.charAt(0) && dfs(b, i, j, w, 0, L)) return true;
-        return false;
-    }
-    private boolean dfs(char[][] b, int r, int c, String w, int idx, int L) {
-        if (idx == L) return true;
-        if (r < 0 || c < 0 || r >= b.length || c >= b[0].length) return false;
-        if (b[r][c] != w.charAt(idx)) return false;
-        char t = b[r][c];
-        b[r][c] = '#';
-        boolean ok = dfs(b, r+1, c, w, idx+1, L) || dfs(b, r-1, c, w, idx+1, L)
-                  || dfs(b, r, c+1, w, idx+1, L) || dfs(b, r, c-1, w, idx+1, L);
-        b[r][c] = t;
-        return ok;
+    public int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        if (n <= 2) return n;
+        int j = 2;
+        for (int i = 2; i < n; i++) {
+            if (nums[i] != nums[j - 2]) nums[j++] = nums[i];
+        }
+        return j;
     }
 }
