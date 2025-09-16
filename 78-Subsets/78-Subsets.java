9e1bc6440226a71752c749d89bcb11825061a36f
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-16 23:06:53 +0530

    File updated at 16/09/2025, 23:06:50

--- 78-Subsets/78-Subsets.java
+++ 78-Subsets/78-Subsets.java
@@ -1,20 +1,17 @@
-// Last updated: 16/09/2025, 23:06:13
-import java.util.*;
-
+// Last updated: 16/09/2025, 23:06:50
 class Solution {
-    public List<List<Integer>> subsetsWithDup(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        Arrays.sort(nums);
-        backtrack(nums, 0, new ArrayList<>(), res);
-        return res;
-    }
-    private void backtrack(int[] nums, int start, List<Integer> cur, List<List<Integer>> res) {
-        res.add(new ArrayList<>(cur));
-        for (int i = start; i < nums.length; i++) {
-            if (i > start && nums[i] == nums[i - 1]) continue;
-            cur.add(nums[i]);
-            backtrack(nums, i + 1, cur, res);
-            cur.remove(cur.size() - 1);
+    public int numDecodings(String s) {
+        if (s == null || s.length() == 0) return 0;
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) == '0' ? 0 : 1;
+        for (int i = 2; i <= n; i++) {
+            char c = s.charAt(i - 1);
+            if (c != '0') dp[i] += dp[i - 1];
+            int two = Integer.parseInt(s.substring(i - 2, i));
+            if (two >= 10 && two <= 26) dp[i] += dp[i - 2];
         }
+        return dp[n];
     }
 }
