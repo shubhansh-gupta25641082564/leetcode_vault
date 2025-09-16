447a7ed6a1fec1b83b479a740ceb180f5dab8657
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-16 23:06:15 +0530

    File updated at 16/09/2025, 23:06:13

--- 78-Subsets/78-Subsets.java
+++ 78-Subsets/78-Subsets.java
@@ -1,11 +1,20 @@
-// Last updated: 16/09/2025, 23:05:26
+// Last updated: 16/09/2025, 23:06:13
 import java.util.*;
 
 class Solution {
-    public List<Integer> grayCode(int n) {
-        List<Integer> res = new ArrayList<>();
-        int size = 1 << n;
-        for (int i = 0; i < size; i++) res.add(i ^ (i >> 1));
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(nums, 0, new ArrayList<>(), res);
         return res;
+    }
+    private void backtrack(int[] nums, int start, List<Integer> cur, List<List<Integer>> res) {
+        res.add(new ArrayList<>(cur));
+        for (int i = start; i < nums.length; i++) {
+            if (i > start && nums[i] == nums[i - 1]) continue;
+            cur.add(nums[i]);
+            backtrack(nums, i + 1, cur, res);
+            cur.remove(cur.size() - 1);
+        }
     }
 }
