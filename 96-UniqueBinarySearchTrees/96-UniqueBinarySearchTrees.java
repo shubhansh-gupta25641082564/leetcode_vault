3988fcac20c7d4336ef23d5c9bbec7e17106a1a5
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-16 23:15:03 +0530

    Create 96-UniqueBinarySearchTrees

---
+++ 96-UniqueBinarySearchTrees/96-UniqueBinarySearchTrees.java
@@ -0,0 +1,13 @@
+// Last updated: 16/09/2025, 23:15:00
+class Solution {
+    public int numTrees(int n) {
+        long[] dp = new long[n + 1];
+        dp[0] = dp[1] = 1;
+        for (int nodes = 2; nodes <= n; nodes++) {
+            for (int root = 1; root <= nodes; root++) {
+                dp[nodes] += dp[root - 1] * dp[nodes - root];
+            }
+        }
+        return (int) dp[n];
+    }
+}
