a5925afb107e65b7e6fe266a039591983096c6f8
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-16 23:10:17 +0530

    File updated at 16/09/2025, 23:10:15

--- 78-Subsets/78-Subsets.java
+++ 78-Subsets/78-Subsets.java
@@ -1,4 +1,4 @@
-// Last updated: 16/09/2025, 23:09:31
+// Last updated: 16/09/2025, 23:10:15
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -13,19 +13,33 @@
 import java.util.*;
 
 class Solution {
-    public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> res = new ArrayList<>();
-        Deque<TreeNode> stack = new ArrayDeque<>();
-        TreeNode curr = root;
-        while (curr != null || !stack.isEmpty()) {
-            while (curr != null) {
-                stack.push(curr);
-                curr = curr.left;
-            }
-            curr = stack.pop();
-            res.add(curr.val);
-            curr = curr.right;
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) return new ArrayList<>();
+        return build(1, n, new HashMap<>());
+    }
+
+    private List<TreeNode> build(int start, int end, Map<String, List<TreeNode>> memo) {
+        String key = start + "#" + end;
+        if (memo.containsKey(key)) return memo.get(key);
+        List<TreeNode> res = new ArrayList<>();
+        if (start > end) {
+            res.add(null);
+            memo.put(key, res);
+            return res;
         }
+        for (int root = start; root <= end; root++) {
+            List<TreeNode> leftList = build(start, root - 1, memo);
+            List<TreeNode> rightList = build(root + 1, end, memo);
+            for (TreeNode left : leftList) {
+                for (TreeNode right : rightList) {
+                    TreeNode node = new TreeNode(root);
+                    node.left = left;
+                    node.right = right;
+                    res.add(node);
+                }
+            }
+        }
+        memo.put(key, res);
         return res;
     }
 }
