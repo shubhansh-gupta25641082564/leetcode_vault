aab0ceaec77182a16f36d5d44e900ed33f628c57
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-16 23:00:42 +0530

    File updated at 16/09/2025, 23:00:39

--- 78-Subsets/78-Subsets.java
+++ 78-Subsets/78-Subsets.java
@@ -1,11 +1,22 @@
-// Last updated: 16/09/2025, 22:59:55
+// Last updated: 16/09/2025, 23:00:39
+import java.util.*;
+
 class Solution {
-    public ListNode deleteDuplicates(ListNode head) {
-        ListNode cur = head;
-        while (cur != null && cur.next != null) {
-            if (cur.val == cur.next.val) cur.next = cur.next.next;
-            else cur = cur.next;
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        Deque<Integer> st = new ArrayDeque<>();
+        long max = 0;
+        for (int i = 0; i <= n; i++) {
+            int h = (i == n ? 0 : heights[i]);
+            while (!st.isEmpty() && heights[st.peekLast()] > h) {
+                int idx = st.pollLast();
+                int left = st.isEmpty() ? -1 : st.peekLast();
+                long width = i - left - 1;
+                long area = (long)heights[idx] * width;
+                if (area > max) max = area;
+            }
+            st.addLast(i);
         }
-        return head;
+        return (int)max;
     }
 }
