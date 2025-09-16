205cbe69b9247682cf603b7fb0900da06ee4a8f9
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-16 23:03:14 +0530

    File updated at 16/09/2025, 23:03:11

--- 78-Subsets/78-Subsets.java
+++ 78-Subsets/78-Subsets.java
@@ -1,27 +1,33 @@
-// Last updated: 16/09/2025, 23:01:30
-import java.util.*;
-
+// Last updated: 16/09/2025, 23:03:11
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-    public int maximalRectangle(char[][] matrix) {
-        if (matrix == null || matrix.length == 0) return 0;
-        int m = matrix.length, n = matrix[0].length;
-        int[] h = new int[n];
-        int max = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) h[j] = matrix[i][j] == '1' ? h[j] + 1 : 0;
-            Deque<Integer> st = new ArrayDeque<>();
-            for (int j = 0; j <= n; j++) {
-                int cur = (j == n) ? 0 : h[j];
-                while (!st.isEmpty() && h[st.peekLast()] > cur) {
-                    int height = h[st.pollLast()];
-                    int left = st.isEmpty() ? -1 : st.peekLast();
-                    int width = j - left - 1;
-                    int area = height * width;
-                    if (area > max) max = area;
-                }
-                st.addLast(j);
+    public ListNode partition(ListNode head, int x) {
+        ListNode lessDummy = new ListNode(0);
+        ListNode greaterDummy = new ListNode(0);
+        ListNode less = lessDummy;
+        ListNode greater = greaterDummy;
+        ListNode curr = head;
+        while (curr != null) {
+            if (curr.val < x) {
+                less.next = curr;
+                less = less.next;
+            } else {
+                greater.next = curr;
+                greater = greater.next;
             }
+            curr = curr.next;
         }
-        return max;
+        greater.next = null;
+        less.next = greaterDummy.next;
+        return lessDummy.next;
     }
 }
