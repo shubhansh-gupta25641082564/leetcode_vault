1efd3d74deb0aa52d467fef80e97630746adfef3
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-16 22:55:06 +0530

    File updated at 16/09/2025, 22:55:03

--- 78-Subsets/78-Subsets.java
+++ 78-Subsets/78-Subsets.java
@@ -1,21 +1,22 @@
-// Last updated: 16/09/2025, 22:54:04
-import java.util.*;
-
+// Last updated: 16/09/2025, 22:55:03
 class Solution {
-    public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        List<Integer> cur = new ArrayList<>();
-        backtrack(0, nums, cur, res);
-        return res;
+    public boolean exist(char[][] b, String w) {
+        int m = b.length, n = b[0].length;
+        int L = w.length();
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                if (b[i][j] == w.charAt(0) && dfs(b, i, j, w, 0, L)) return true;
+        return false;
     }
-    private void backtrack(int idx, int[] nums, List<Integer> cur, List<List<Integer>> res) {
-        if (idx == nums.length) {
-            res.add(new ArrayList<>(cur));
-            return;
-        }
-        backtrack(idx + 1, nums, cur, res);
-        cur.add(nums[idx]);
-        backtrack(idx + 1, nums, cur, res);
-        cur.remove(cur.size() - 1);
+    private boolean dfs(char[][] b, int r, int c, String w, int idx, int L) {
+        if (idx == L) return true;
+        if (r < 0 || c < 0 || r >= b.length || c >= b[0].length) return false;
+        if (b[r][c] != w.charAt(idx)) return false;
+        char t = b[r][c];
+        b[r][c] = '#';
+        boolean ok = dfs(b, r+1, c, w, idx+1, L) || dfs(b, r-1, c, w, idx+1, L)
+                  || dfs(b, r, c+1, w, idx+1, L) || dfs(b, r, c-1, w, idx+1, L);
+        b[r][c] = t;
+        return ok;
     }
 }
