3ddab73d8596f9cf02d0b2f032efdf659c65c873
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-16 23:04:37 +0530

    File updated at 16/09/2025, 23:04:35

--- 78-Subsets/78-Subsets.java
+++ 78-Subsets/78-Subsets.java
@@ -1,37 +1,13 @@
-// Last updated: 16/09/2025, 23:03:50
+// Last updated: 16/09/2025, 23:04:35
 class Solution {
-    public boolean isScramble(String s1, String s2) {
-        if (s1.length() != s2.length()) return false;
-        return dfs(s1, s2, new java.util.HashMap<>());
-    }
-    private boolean dfs(String a, String b, java.util.Map<String, Boolean> memo) {
-        String key = a + "#" + b;
-        if (memo.containsKey(key)) return memo.get(key);
-        if (a.equals(b)) {
-            memo.put(key, true);
-            return true;
-        }
-        int n = a.length();
-        int[] cnt = new int[26];
-        for (int i = 0; i < n; i++) {
-            cnt[a.charAt(i) - 'a']++;
-            cnt[b.charAt(i) - 'a']--;
-        }
-        for (int v : cnt) if (v != 0) {
-            memo.put(key, false);
-            return false;
-        }
-        for (int i = 1; i < n; i++) {
-            if (dfs(a.substring(0, i), b.substring(0, i), memo) && dfs(a.substring(i), b.substring(i), memo)) {
-                memo.put(key, true);
-                return true;
-            }
-            if (dfs(a.substring(0, i), b.substring(n - i), memo) && dfs(a.substring(i), b.substring(0, n - i), memo)) {
-                memo.put(key, true);
-                return true;
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m - 1, p2 = n - 1, p = m + n - 1;
+        while (p2 >= 0) {
+            if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+                nums1[p--] = nums1[p1--];
+            } else {
+                nums1[p--] = nums2[p2--];
             }
         }
-        memo.put(key, false);
-        return false;
     }
 }
