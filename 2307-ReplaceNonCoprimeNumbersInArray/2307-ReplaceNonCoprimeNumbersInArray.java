353c1b54a955bab5efefe6e1caa3a41f9942e783
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-16 23:14:51 +0530

    Create 2307-ReplaceNonCoprimeNumbersInArray

---
+++ 2307-ReplaceNonCoprimeNumbersInArray/2307-ReplaceNonCoprimeNumbersInArray.java
@@ -0,0 +1,30 @@
+// Last updated: 16/09/2025, 23:14:49
+import java.util.*;
+
+class Solution {
+    public List<Integer> replaceNonCoprimes(int[] nums) {
+        Deque<Long> st = new ArrayDeque<>();
+        for (int v : nums) {
+            long cur = v;
+            while (!st.isEmpty()) {
+                long top = st.peekLast();
+                long g = gcd(top, cur);
+                if (g == 1) break;
+                st.pollLast();
+                cur = top / g * cur;
+            }
+            st.addLast(cur);
+        }
+        List<Integer> res = new ArrayList<>(st.size());
+        for (long x : st) res.add((int)x);
+        return res;
+    }
+    private long gcd(long a, long b) {
+        while (b != 0) {
+            long t = a % b;
+            a = b;
+            b = t;
+        }
+        return a;
+    }
+}
