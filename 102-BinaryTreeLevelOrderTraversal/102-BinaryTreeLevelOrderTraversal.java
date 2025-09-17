a3c0569a2c8f3797b7d12717d06dc6f4ef8055f0
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-17 22:47:20 +0530

    File updated at 17/09/2025, 22:47:17

--- 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.java
+++ 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.java
@@ -1,23 +1,23 @@
-// Last updated: 17/09/2025, 22:45:41
+// Last updated: 17/09/2025, 22:47:17
 import java.util.*;
 
 class Solution {
     private Map<Integer,Integer> idxMap;
 
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
         idxMap = new HashMap<>();
         for (int i = 0; i < inorder.length; i++) idxMap.put(inorder[i], i);
-        return build(preorder, 0, preorder.length - 1, 0, inorder.length - 1);
+        return build(postorder, 0, postorder.length - 1, 0, inorder.length - 1);
     }
 
-    private TreeNode build(int[] pre, int preL, int preR, int inL, int inR) {
-        if (preL > preR || inL > inR) return null;
-        int rootVal = pre[preL];
+    private TreeNode build(int[] post, int postL, int postR, int inL, int inR) {
+        if (postL > postR || inL > inR) return null;
+        int rootVal = post[postR];
         TreeNode root = new TreeNode(rootVal);
         int inIndex = idxMap.get(rootVal);
         int leftSize = inIndex - inL;
-        root.left = build(pre, preL + 1, preL + leftSize, inL, inIndex - 1);
-        root.right = build(pre, preL + leftSize + 1, preR, inIndex + 1, inR);
+        root.left = build(post, postL, postL + leftSize - 1, inL, inIndex - 1);
+        root.right = build(post, postL + leftSize, postR - 1, inIndex + 1, inR);
         return root;
     }
 }
