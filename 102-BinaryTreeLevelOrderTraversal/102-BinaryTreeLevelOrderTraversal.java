aa8d1d429272b8ce177350f302e2bd17a86e50b3
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-17 22:45:44 +0530

    File updated at 17/09/2025, 22:45:41

--- 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.java
+++ 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.java
@@ -1,9 +1,23 @@
-// Last updated: 17/09/2025, 22:42:53
+// Last updated: 17/09/2025, 22:45:41
+import java.util.*;
+
 class Solution {
-    public int maxDepth(TreeNode root) {
-        if (root == null) return 0;
-        int left = maxDepth(root.left);
-        int right = maxDepth(root.right);
-        return 1 + Math.max(left, right);
+    private Map<Integer,Integer> idxMap;
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        idxMap = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++) idxMap.put(inorder[i], i);
+        return build(preorder, 0, preorder.length - 1, 0, inorder.length - 1);
+    }
+
+    private TreeNode build(int[] pre, int preL, int preR, int inL, int inR) {
+        if (preL > preR || inL > inR) return null;
+        int rootVal = pre[preL];
+        TreeNode root = new TreeNode(rootVal);
+        int inIndex = idxMap.get(rootVal);
+        int leftSize = inIndex - inL;
+        root.left = build(pre, preL + 1, preL + leftSize, inL, inIndex - 1);
+        root.right = build(pre, preL + leftSize + 1, preR, inIndex + 1, inR);
+        return root;
     }
 }
