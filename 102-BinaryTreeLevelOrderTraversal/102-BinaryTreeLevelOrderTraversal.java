8b198c0560d0351ea1a77d345ca4abf8ccfb64c3
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-17 22:38:57 +0530

    File updated at 17/09/2025, 22:38:54

--- 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.java
+++ 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.java
@@ -1,24 +1,28 @@
-// Last updated: 17/09/2025, 22:37:33
+// Last updated: 17/09/2025, 22:38:54
 import java.util.*;
 
 class Solution {
-    public List<List<Integer>> levelOrder(TreeNode root) {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> res = new ArrayList<>();
         if (root == null) return res;
 
         Queue<TreeNode> q = new ArrayDeque<>();
         q.add(root);
+        boolean leftToRight = true;
 
         while (!q.isEmpty()) {
             int sz = q.size();
-            List<Integer> level = new ArrayList<>(sz);
+            LinkedList<Integer> level = new LinkedList<>();
             for (int i = 0; i < sz; i++) {
                 TreeNode node = q.poll();
-                level.add(node.val);
+                if (leftToRight) level.addLast(node.val);
+                else level.addFirst(node.val);
+
                 if (node.left != null) q.add(node.left);
                 if (node.right != null) q.add(node.right);
             }
             res.add(level);
+            leftToRight = !leftToRight;
         }
 
         return res;
