865c98e7b034d645cd59ba7ee27c8fc382f242f9
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-17 22:57:19 +0530

    File updated at 17/09/2025, 22:57:17

--- 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.java
+++ 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.java
@@ -1,23 +1,26 @@
-// Last updated: 17/09/2025, 22:47:17
+// Last updated: 17/09/2025, 22:57:17
 import java.util.*;
 
 class Solution {
-    private Map<Integer,Integer> idxMap;
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        LinkedList<List<Integer>> res = new LinkedList<>();
+        if (root == null) return res;
 
-    public TreeNode buildTree(int[] inorder, int[] postorder) {
-        idxMap = new HashMap<>();
-        for (int i = 0; i < inorder.length; i++) idxMap.put(inorder[i], i);
-        return build(postorder, 0, postorder.length - 1, 0, inorder.length - 1);
-    }
+        Queue<TreeNode> q = new ArrayDeque<>();
+        q.add(root);
 
-    private TreeNode build(int[] post, int postL, int postR, int inL, int inR) {
-        if (postL > postR || inL > inR) return null;
-        int rootVal = post[postR];
-        TreeNode root = new TreeNode(rootVal);
-        int inIndex = idxMap.get(rootVal);
-        int leftSize = inIndex - inL;
-        root.left = build(post, postL, postL + leftSize - 1, inL, inIndex - 1);
-        root.right = build(post, postL + leftSize, postR - 1, inIndex + 1, inR);
-        return root;
+        while (!q.isEmpty()) {
+            int sz = q.size();
+            List<Integer> level = new ArrayList<>(sz);
+            for (int i = 0; i < sz; i++) {
+                TreeNode node = q.poll();
+                level.add(node.val);
+                if (node.left != null) q.add(node.left);
+                if (node.right != null) q.add(node.right);
+            }
+            res.addFirst(level);
+        }
+
+        return res;
     }
 }
