ab77f11499cc630bf7ca47d2b1b6f2d01efaf622
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-17 23:05:57 +0530

    File updated at 17/09/2025, 23:05:55

--- 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.java
+++ 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.java
@@ -1,38 +1,21 @@
-// Last updated: 17/09/2025, 23:05:17
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-
+// Last updated: 17/09/2025, 23:05:55
 class Solution {
-    public void flatten(TreeNode root) {
-        TreeNode curr = root;
-        while (curr != null) {
-            if (curr.left != null) {
-                // find rightmost node of left subtree
-                TreeNode pred = curr.left;
-                while (pred.right != null) {
-                    pred = pred.right;
+    public int numDistinct(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+        // use long to be safe for intermediate sums; final answer fits in 32-bit per problem statement
+        long[] dp = new long[m + 1];
+        dp[0] = 1; // one way to form empty t
+
+        for (int i = 1; i <= n; i++) {
+            // iterate j from m down to 1 to avoid overwriting dp[j-1] needed for this row
+            for (int j = m; j >= 1; j--) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[j] += dp[j - 1];
                 }
-                // link predecessor's right to curr.right
-                pred.right = curr.right;
-                // move left subtree to right
-                curr.right = curr.left;
-                curr.left = null;
             }
-            // advance to next node (always go right in the flattened list)
-            curr = curr.right;
         }
+
+        return (int) dp[m];
     }
 }
