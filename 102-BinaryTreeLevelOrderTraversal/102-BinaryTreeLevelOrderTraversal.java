6e525879ed7b51bfc8cc15558a91f68197dd15f9
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-17 23:07:50 +0530

    File updated at 17/09/2025, 23:07:48

--- 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.java
+++ 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.java
@@ -1,16 +1,26 @@
-// Last updated: 17/09/2025, 23:06:48
+// Last updated: 17/09/2025, 23:07:48
 class Solution {
     public Node connect(Node root) {
-        if (root == null) return null;
-        Node levelStart = root;
-        while (levelStart.left != null) {
-            Node curr = levelStart;
+        Node curr = root;
+        Node head = null; // head of next level
+        Node prev = null; // the leading node on the next level
+        while (curr != null) {
             while (curr != null) {
-                curr.left.next = curr.right;
-                if (curr.next != null) curr.right.next = curr.next.left;
+                if (curr.left != null) {
+                    if (prev != null) prev.next = curr.left;
+                    else head = curr.left;
+                    prev = curr.left;
+                }
+                if (curr.right != null) {
+                    if (prev != null) prev.next = curr.right;
+                    else head = curr.right;
+                    prev = curr.right;
+                }
                 curr = curr.next;
             }
-            levelStart = levelStart.left;
+            curr = head;
+            head = null;
+            prev = null;
         }
         return root;
     }
