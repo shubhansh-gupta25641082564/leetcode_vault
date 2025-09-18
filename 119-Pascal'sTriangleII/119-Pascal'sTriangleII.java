bfd1bc97cc3a88ddf268086d269e51ee1509106c
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-18 22:59:43 +0530

    File updated at 18/09/2025, 22:59:40

--- 119-Pascal'sTriangleII/119-Pascal'sTriangleII.java
+++ 119-Pascal'sTriangleII/119-Pascal'sTriangleII.java
@@ -1,15 +1,17 @@
-// Last updated: 18/09/2025, 22:58:52
+// Last updated: 18/09/2025, 22:59:40
 public class Solution {
-    private int maxSum = Integer.MIN_VALUE;
-    public int maxPathSum(TreeNode root) {
-        maxGain(root);
-        return maxSum;
-    }
-    private int maxGain(TreeNode node) {
-        if (node == null) return 0;
-        int left = Math.max(0, maxGain(node.left));
-        int right = Math.max(0, maxGain(node.right));
-        maxSum = Math.max(maxSum, node.val + left + right);
-        return node.val + Math.max(left, right);
+    public boolean isPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            while (i < j && !Character.isLetterOrDigit(s.charAt(i))) i++;
+            while (i < j && !Character.isLetterOrDigit(s.charAt(j))) j--;
+            if (i < j) {
+                char a = Character.toLowerCase(s.charAt(i));
+                char b = Character.toLowerCase(s.charAt(j));
+                if (a != b) return false;
+                i++; j--;
+            }
+        }
+        return true;
     }
 }
