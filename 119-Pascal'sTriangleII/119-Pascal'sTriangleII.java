1d1edc8264c63e6cd9c860090d03c50b6f9930d8
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-18 23:15:37 +0530

    File updated at 18/09/2025, 23:15:35

--- 119-Pascal'sTriangleII/119-Pascal'sTriangleII.java
+++ 119-Pascal'sTriangleII/119-Pascal'sTriangleII.java
@@ -1,39 +1,31 @@
-// Last updated: 18/09/2025, 23:14:53
+// Last updated: 18/09/2025, 23:15:35
 import java.util.*;
 
 public class Solution {
-    public void solve(char[][] board) {
-        if (board == null || board.length == 0) return;
-        int m = board.length, n = board[0].length;
-        Queue<int[]> q = new LinkedList<>();
-
-        // add all border 'O's to queue and mark them as safe 'S'
-        for (int i = 0; i < m; i++) {
-            if (board[i][0] == 'O') { q.add(new int[]{i, 0}); board[i][0] = 'S'; }
-            if (board[i][n - 1] == 'O') { q.add(new int[]{i, n - 1}); board[i][n - 1] = 'S'; }
-        }
-        for (int j = 0; j < n; j++) {
-            if (board[0][j] == 'O') { q.add(new int[]{0, j}); board[0][j] = 'S'; }
-            if (board[m - 1][j] == 'O') { q.add(new int[]{m - 1, j}); board[m - 1][j] = 'S'; }
-        }
-
-        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            for (int[] d : dirs) {
-                int ni = cur[0] + d[0], nj = cur[1] + d[1];
-                if (ni >= 0 && ni < m && nj >= 0 && nj < n && board[ni][nj] == 'O') {
-                    board[ni][nj] = 'S';
-                    q.add(new int[]{ni, nj});
-                }
+    public List<List<String>> partition(String s) {
+        int n = s.length();
+        boolean[][] pal = new boolean[n][n];
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+                pal[i][j] = s.charAt(i) == s.charAt(j) && (j - i < 2 || pal[i + 1][j - 1]);
             }
         }
+        List<List<String>> res = new ArrayList<>();
+        dfs(0, s, pal, new ArrayList<>(), res);
+        return res;
+    }
 
-        // flip surrounded 'O' -> 'X', and restore safe 'S' -> 'O'
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == 'O') board[i][j] = 'X';
-                else if (board[i][j] == 'S') board[i][j] = 'O';
+    private void dfs(int idx, String s, boolean[][] pal, List<String> cur, List<List<String>> res) {
+        int n = s.length();
+        if (idx == n) {
+            res.add(new ArrayList<>(cur));
+            return;
+        }
+        for (int j = idx; j < n; j++) {
+            if (pal[idx][j]) {
+                cur.add(s.substring(idx, j + 1));
+                dfs(j + 1, s, pal, cur, res);
+                cur.remove(cur.size() - 1);
             }
         }
     }
