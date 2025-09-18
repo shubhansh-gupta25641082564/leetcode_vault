519a9db5d5781693fef66bb327236e08759c1640
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-18 23:14:55 +0530

    File updated at 18/09/2025, 23:14:53

--- 119-Pascal'sTriangleII/119-Pascal'sTriangleII.java
+++ 119-Pascal'sTriangleII/119-Pascal'sTriangleII.java
@@ -1,26 +1,40 @@
-// Last updated: 18/09/2025, 23:14:01
+// Last updated: 18/09/2025, 23:14:53
+import java.util.*;
+
 public class Solution {
-    public int sumNumbers(TreeNode root) {
-        return dfs(root, 0);
-    }
+    public void solve(char[][] board) {
+        if (board == null || board.length == 0) return;
+        int m = board.length, n = board[0].length;
+        Queue<int[]> q = new LinkedList<>();
 
-    private int dfs(TreeNode node, int curr) {
-        if (node == null) return 0;
-        curr = curr * 10 + node.val;
-        if (node.left == null && node.right == null) return curr;
-        return dfs(node.left, curr) + dfs(node.right, curr);
-    }
-}
+        // add all border 'O's to queue and mark them as safe 'S'
+        for (int i = 0; i < m; i++) {
+            if (board[i][0] == 'O') { q.add(new int[]{i, 0}); board[i][0] = 'S'; }
+            if (board[i][n - 1] == 'O') { q.add(new int[]{i, n - 1}); board[i][n - 1] = 'S'; }
+        }
+        for (int j = 0; j < n; j++) {
+            if (board[0][j] == 'O') { q.add(new int[]{0, j}); board[0][j] = 'S'; }
+            if (board[m - 1][j] == 'O') { q.add(new int[]{m - 1, j}); board[m - 1][j] = 'S'; }
+        }
 
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode() {}
-    TreeNode(int val) { this.val = val; }
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
+        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            for (int[] d : dirs) {
+                int ni = cur[0] + d[0], nj = cur[1] + d[1];
+                if (ni >= 0 && ni < m && nj >= 0 && nj < n && board[ni][nj] == 'O') {
+                    board[ni][nj] = 'S';
+                    q.add(new int[]{ni, nj});
+                }
+            }
+        }
+
+        // flip surrounded 'O' -> 'X', and restore safe 'S' -> 'O'
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') board[i][j] = 'X';
+                else if (board[i][j] == 'S') board[i][j] = 'O';
+            }
+        }
     }
 }
