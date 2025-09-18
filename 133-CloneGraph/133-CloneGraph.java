2cb81b044e661b63215418b217ddc119b16d8735
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-18 23:30:32 +0530

    Create 133-CloneGraph

---
+++ 133-CloneGraph/133-CloneGraph.java
@@ -0,0 +1,23 @@
+// Last updated: 18/09/2025, 23:30:29
+import java.util.*;
+
+public class Solution {
+    public Node cloneGraph(Node node) {
+        if (node == null) return null;
+        Map<Node, Node> map = new HashMap<>();
+        Queue<Node> q = new LinkedList<>();
+        q.add(node);
+        map.put(node, new Node(node.val, new ArrayList<>()));
+        while (!q.isEmpty()) {
+            Node cur = q.poll();
+            for (Node nei : cur.neighbors) {
+                if (!map.containsKey(nei)) {
+                    map.put(nei, new Node(nei.val, new ArrayList<>()));
+                    q.add(nei);
+                }
+                map.get(cur).neighbors.add(map.get(nei));
+            }
+        }
+        return map.get(node);
+    }
+}
