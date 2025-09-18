b95f3afac4ab49917d367857f99cda790c770b8a
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-18 23:30:26 +0530

    Create 137-SingleNumberIi

---
+++ 137-SingleNumberIi/137-SingleNumberIi.java
@@ -0,0 +1,18 @@
+// Last updated: 18/09/2025, 23:30:23
+public class Solution {
+    public int singleNumber(int[] nums) {
+        int[] bits = new int[32];
+        for (int num : nums) {
+            for (int i = 0; i < 32; i++) {
+                bits[i] += (num >>> i) & 1; // use unsigned shift
+            }
+        }
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            if (bits[i] % 3 != 0) {
+                result |= (1 << i);
+            }
+        }
+        return result;
+    }
+}
