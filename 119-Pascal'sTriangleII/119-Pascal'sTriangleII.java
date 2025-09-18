6c289f633c33a73707b0461d92cf9f936924c7b2
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-18 22:55:21 +0530

    File updated at 18/09/2025, 22:55:18

--- 119-Pascal'sTriangleII/119-Pascal'sTriangleII.java
+++ 119-Pascal'sTriangleII/119-Pascal'sTriangleII.java
@@ -1,10 +1,16 @@
-// Last updated: 18/09/2025, 22:51:45
+// Last updated: 18/09/2025, 22:55:18
 public class Solution {
     public int maxProfit(int[] prices) {
-        int profit = 0;
-        for (int i = 1; i < prices.length; i++) {
-            if (prices[i] > prices[i - 1]) profit += prices[i] - prices[i - 1];
+        int firstBuy = Integer.MAX_VALUE;
+        int firstProfit = 0;
+        int secondBuy = Integer.MAX_VALUE;
+        int secondProfit = 0;
+        for (int p : prices) {
+            firstBuy = Math.min(firstBuy, p);
+            firstProfit = Math.max(firstProfit, p - firstBuy);
+            secondBuy = Math.min(secondBuy, p - firstProfit);
+            secondProfit = Math.max(secondProfit, p - secondBuy);
         }
-        return profit;
+        return secondProfit;
     }
 }
