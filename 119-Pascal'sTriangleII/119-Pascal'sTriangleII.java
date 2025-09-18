91e6fb4fe87037ba32c3619f70d0676f57c52711
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-18 22:58:55 +0530

    File updated at 18/09/2025, 22:58:52

--- 119-Pascal'sTriangleII/119-Pascal'sTriangleII.java
+++ 119-Pascal'sTriangleII/119-Pascal'sTriangleII.java
@@ -1,16 +1,15 @@
-// Last updated: 18/09/2025, 22:55:18
+// Last updated: 18/09/2025, 22:58:52
 public class Solution {
-    public int maxProfit(int[] prices) {
-        int firstBuy = Integer.MAX_VALUE;
-        int firstProfit = 0;
-        int secondBuy = Integer.MAX_VALUE;
-        int secondProfit = 0;
-        for (int p : prices) {
-            firstBuy = Math.min(firstBuy, p);
-            firstProfit = Math.max(firstProfit, p - firstBuy);
-            secondBuy = Math.min(secondBuy, p - firstProfit);
-            secondProfit = Math.max(secondProfit, p - secondBuy);
-        }
-        return secondProfit;
+    private int maxSum = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        maxGain(root);
+        return maxSum;
+    }
+    private int maxGain(TreeNode node) {
+        if (node == null) return 0;
+        int left = Math.max(0, maxGain(node.left));
+        int right = Math.max(0, maxGain(node.right));
+        maxSum = Math.max(maxSum, node.val + left + right);
+        return node.val + Math.max(left, right);
     }
 }
