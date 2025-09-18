faf75390e449d010401fb6ee5c2100022062c7bf
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-18 23:30:15 +0530

    Create 143-ReorderList

---
+++ 143-ReorderList/143-ReorderList.java
@@ -0,0 +1,38 @@
+// Last updated: 18/09/2025, 23:30:12
+public class Solution {
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null) return;
+
+        // 1. find middle (slow will point to end of first half)
+        ListNode slow = head, fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // 2. split and reverse second half
+        ListNode second = slow.next;
+        slow.next = null; // terminate first half
+        ListNode prev = null;
+        while (second != null) {
+            ListNode nxt = second.next;
+            second.next = prev;
+            prev = second;
+            second = nxt;
+        }
+        // now prev is head of reversed second half
+
+        // 3. merge first half (head) and reversed second half (prev)
+        ListNode first = head;
+        while (prev != null) {
+            ListNode n1 = first.next;
+            ListNode n2 = prev.next;
+
+            first.next = prev;
+            prev.next = n1;
+
+            first = n1;
+            prev = n2;
+        }
+    }
+}
