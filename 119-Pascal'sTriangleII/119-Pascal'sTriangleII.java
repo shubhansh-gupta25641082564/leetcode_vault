df660ccb30f065ca6a2dfad5083a7b34d25014a7
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-18 23:19:03 +0530

    File updated at 18/09/2025, 23:19:00

--- 119-Pascal'sTriangleII/119-Pascal'sTriangleII.java
+++ 119-Pascal'sTriangleII/119-Pascal'sTriangleII.java
@@ -1,32 +1,8 @@
-// Last updated: 18/09/2025, 23:18:32
-import java.util.Arrays;
-
+// Last updated: 18/09/2025, 23:19:00
 public class Solution {
-    public int candy(int[] ratings) {
-        if (ratings == null || ratings.length == 0) return 0;
-        int n = ratings.length;
-        int[] left = new int[n];
-        Arrays.fill(left, 1);
-
-        // left-to-right: satisfy left neighbor condition
-        for (int i = 1; i < n; i++) {
-            if (ratings[i] > ratings[i - 1]) {
-                left[i] = left[i - 1] + 1;
-            }
-        }
-
-        // right-to-left: satisfy right neighbor condition and accumulate result
-        int res = 0;
-        int right = 1; // candies needed considering only the right neighbor
-        for (int i = n - 1; i >= 0; i--) {
-            if (i < n - 1 && ratings[i] > ratings[i + 1]) {
-                right = right + 1;
-            } else if (i < n - 1) {
-                right = 1;
-            }
-            res += Math.max(left[i], right);
-        }
-
-        return res;
+    public int singleNumber(int[] nums) {
+        int ans = 0;
+        for (int x : nums) ans ^= x;
+        return ans;
     }
 }
