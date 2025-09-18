faf87a2180d5cb3a5596b1eb4dbab72a8230ac51
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-18 23:30:04 +0530

    Create 150-EvaluateReversePolishNotation

---
+++ 150-EvaluateReversePolishNotation/150-EvaluateReversePolishNotation.java
@@ -0,0 +1,30 @@
+// Last updated: 18/09/2025, 23:30:02
+import java.util.*;
+
+public class Solution {
+    public int evalRPN(String[] tokens) {
+        Deque<Integer> st = new ArrayDeque<>();
+        for (String t : tokens) {
+            if (t.equals("+")) {
+                int b = st.pop();
+                int a = st.pop();
+                st.push(a + b);
+            } else if (t.equals("-")) {
+                int b = st.pop();
+                int a = st.pop();
+                st.push(a - b);
+            } else if (t.equals("*")) {
+                int b = st.pop();
+                int a = st.pop();
+                st.push(a * b);
+            } else if (t.equals("/")) {
+                int b = st.pop();
+                int a = st.pop();
+                st.push(a / b); // integer division truncates toward zero in Java
+            } else {
+                st.push(Integer.parseInt(t));
+            }
+        }
+        return st.pop();
+    }
+}
