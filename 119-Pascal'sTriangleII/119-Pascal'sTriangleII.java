d107486722e4e19b31f1c98e5ec3489705f9093a
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-18 23:19:31 +0530

    File updated at 18/09/2025, 23:19:28

--- 119-Pascal'sTriangleII/119-Pascal'sTriangleII.java
+++ 119-Pascal'sTriangleII/119-Pascal'sTriangleII.java
@@ -1,8 +1,18 @@
-// Last updated: 18/09/2025, 23:19:00
+// Last updated: 18/09/2025, 23:19:28
 public class Solution {
     public int singleNumber(int[] nums) {
-        int ans = 0;
-        for (int x : nums) ans ^= x;
-        return ans;
+        int[] bits = new int[32];
+        for (int num : nums) {
+            for (int i = 0; i < 32; i++) {
+                bits[i] += (num >>> i) & 1; // use unsigned shift
+            }
+        }
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            if (bits[i] % 3 != 0) {
+                result |= (1 << i);
+            }
+        }
+        return result;
     }
 }
