313d461e03701f3a3fe3c235a8281fdfd1c6ad7d
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-18 23:30:37 +0530

    Create 129-SumRootToLeafNumbers

---
+++ 129-SumRootToLeafNumbers/129-SumRootToLeafNumbers.java
@@ -0,0 +1,26 @@
+// Last updated: 18/09/2025, 23:30:35
+public class Solution {
+    public int sumNumbers(TreeNode root) {
+        return dfs(root, 0);
+    }
+
+    private int dfs(TreeNode node, int curr) {
+        if (node == null) return 0;
+        curr = curr * 10 + node.val;
+        if (node.left == null && node.right == null) return curr;
+        return dfs(node.left, curr) + dfs(node.right, curr);
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
