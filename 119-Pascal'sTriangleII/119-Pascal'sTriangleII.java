9dc6fcd0433b6e94f91d88bf65f2b62c68ea1a3b
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-18 22:47:53 +0530

    File updated at 18/09/2025, 22:47:51

--- 119-Pascal'sTriangleII/119-Pascal'sTriangleII.java
+++ 119-Pascal'sTriangleII/119-Pascal'sTriangleII.java
@@ -1,16 +1,16 @@
-// Last updated: 18/09/2025, 22:46:51
+// Last updated: 18/09/2025, 22:47:51
 import java.util.*;
 
 public class Solution {
-    public List<Integer> getRow(int rowIndex) {
-        List<Integer> row = new ArrayList<>();
-        row.add(1);
-        for (int i = 1; i <= rowIndex; i++) {
-            row.add(1);
-            for (int j = i - 1; j >= 1; j--) {
-                row.set(j, row.get(j) + row.get(j - 1));
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[] dp = new int[n];
+        for (int j = 0; j < n; j++) dp[j] = triangle.get(n - 1).get(j);
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                dp[j] = triangle.get(i).get(j) + Math.min(dp[j], dp[j + 1]);
             }
         }
-        return row;
+        return dp[0];
     }
 }
