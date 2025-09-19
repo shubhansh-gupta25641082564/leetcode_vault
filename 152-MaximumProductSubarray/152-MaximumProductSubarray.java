78ff33ce8286f7443146ffab6794ba910abf2e8b
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-20 00:09:18 +0530

    File updated at 20/09/2025, 00:09:18

--- 152-MaximumProductSubarray/152-MaximumProductSubarray.java
+++ 152-MaximumProductSubarray/152-MaximumProductSubarray.java
@@ -1,17 +1,34 @@
-// Last updated: 20/09/2025, 00:08:33
-class Solution {
-    public int findMin(int[] nums) {
-        int low = 0, high = nums.length - 1;
-        while (low < high) {
-            int mid = low + (high - low) / 2;
-            if (nums[mid] > nums[high]) {
-                low = mid + 1;
-            } else if (nums[mid] < nums[high]) {
-                high = mid;
-            } else { // nums[mid] == nums[high]
-                high--;
-            }
+// Last updated: 20/09/2025, 00:09:18
+import java.util.Stack;
+
+class MinStack {
+    private final Stack<Integer> stack;
+    private final Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
         }
-        return nums[low];
+    }
+
+    public void pop() {
+        int x = stack.pop();
+        if (x == minStack.peek()) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
     }
 }
