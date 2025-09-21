21f6b55735eef76a0f9db26c80a7158838a6270c
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-21 17:15:26 +0530

    File updated at 21/09/2025, 17:15:26

--- 206-ReverseLinkedList/206-ReverseLinkedList.java
+++ 206-ReverseLinkedList/206-ReverseLinkedList.java
@@ -1,18 +1,29 @@
-// Last updated: 21/09/2025, 17:14:51
+// Last updated: 21/09/2025, 17:15:26
 import java.util.*;
 
 class Solution {
-    public List<String> summaryRanges(int[] nums) {
-        List<String> res = new ArrayList<>();
-        int n = nums.length, i = 0;
-        while (i < n) {
-            int start = nums[i];
-            while (i + 1 < n && nums[i + 1] == nums[i] + 1) i++;
-            int end = nums[i];
-            if (start == end) res.add(String.valueOf(start));
-            else res.add(start + "->" + end);
-            i++;
+    public List<Integer> majorityElement(int[] nums) {
+        Integer candidate1 = null, candidate2 = null;
+        int count1 = 0, count2 = 0;
+        
+        for (int num : nums) {
+            if (candidate1 != null && num == candidate1.intValue()) count1++;
+            else if (candidate2 != null && num == candidate2.intValue()) count2++;
+            else if (count1 == 0) { candidate1 = num; count1 = 1; }
+            else if (count2 == 0) { candidate2 = num; count2 = 1; }
+            else { count1--; count2--; }
         }
+        
+        count1 = 0; count2 = 0;
+        for (int num : nums) {
+            if (candidate1 != null && num == candidate1.intValue()) count1++;
+            else if (candidate2 != null && num == candidate2.intValue()) count2++;
+        }
+        
+        List<Integer> res = new ArrayList<>();
+        int n = nums.length;
+        if (count1 > n / 3) res.add(candidate1);
+        if (count2 > n / 3) res.add(candidate2);
         return res;
     }
 }
