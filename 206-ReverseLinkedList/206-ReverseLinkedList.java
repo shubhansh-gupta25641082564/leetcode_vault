8d806b4b6e61c7cb6fd3cd33692f578a2b64e4a6
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-21 16:56:27 +0530

    File updated at 21/09/2025, 16:56:27

--- 206-ReverseLinkedList/206-ReverseLinkedList.java
+++ 206-ReverseLinkedList/206-ReverseLinkedList.java
@@ -1,35 +1,23 @@
-// Last updated: 21/09/2025, 16:55:50
+// Last updated: 21/09/2025, 16:56:27
 import java.util.*;
 
 class Solution {
-    private final Random rand = new Random();
-    public int findKthLargest(int[] nums, int k) {
-        int n = nums.length;
-        int target = n - k;
-        int lo = 0, hi = n - 1;
-        while (lo <= hi) {
-            int pivotIndex = lo + rand.nextInt(hi - lo + 1);
-            int pos = partition(nums, lo, hi, pivotIndex);
-            if (pos == target) return nums[pos];
-            else if (pos < target) lo = pos + 1;
-            else hi = pos - 1;
-        }
-        return -1;
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(1, k, n, new ArrayList<>(), res);
+        return res;
     }
-    private int partition(int[] a, int l, int r, int pivotIdx) {
-        int pv = a[pivotIdx];
-        swap(a, pivotIdx, r);
-        int store = l;
-        for (int i = l; i < r; ++i) {
-            if (a[i] < pv) {
-                swap(a, store, i);
-                store++;
-            }
+    private void backtrack(int start, int k, int target, List<Integer> cur, List<List<Integer>> res) {
+        if (target == 0 && k == 0) {
+            res.add(new ArrayList<>(cur));
+            return;
         }
-        swap(a, store, r);
-        return store;
-    }
-    private void swap(int[] a, int i, int j) {
-        int t = a[i]; a[i] = a[j]; a[j] = t;
+        if (k == 0) return;
+        for (int i = start; i <= 9; ++i) {
+            if (i > target) break;
+            cur.add(i);
+            backtrack(i + 1, k - 1, target - i, cur, res);
+            cur.remove(cur.size() - 1);
+        }
     }
 }
