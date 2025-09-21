ee7256fea8ab55f54e7491751771490a07e0f863
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-21 16:59:22 +0530

    File updated at 21/09/2025, 16:59:21

--- 206-ReverseLinkedList/206-ReverseLinkedList.java
+++ 206-ReverseLinkedList/206-ReverseLinkedList.java
@@ -1,37 +1,16 @@
-// Last updated: 21/09/2025, 16:57:45
+// Last updated: 21/09/2025, 16:59:21
 import java.util.*;
 
 class Solution {
-    public List<List<Integer>> getSkyline(int[][] buildings) {
-        List<int[]> events = new ArrayList<>();
-        for (int[] b : buildings) {
-            events.add(new int[]{b[0], -b[2]}); // entering, height negative
-            events.add(new int[]{b[1], b[2]});  // leaving, height positive
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int indexDiff, int valueDiff) {
+        TreeSet<Long> set = new TreeSet<>();
+        for (int i = 0; i < nums.length; ++i) {
+            Long floor = set.floor((long) nums[i] + valueDiff);
+            Long ceil = set.ceiling((long) nums[i] - valueDiff);
+            if ((floor != null && floor >= nums[i]) || (ceil != null && ceil <= nums[i])) return true;
+            set.add((long) nums[i]);
+            if (set.size() > indexDiff) set.remove((long) nums[i - indexDiff]);
         }
-        events.sort((a, b) -> {
-            if (a[0] != b[0]) return a[0] - b[0];
-            return a[1] - b[1];
-        });
-
-        TreeMap<Integer, Integer> map = new TreeMap<>(Collections.reverseOrder());
-        map.put(0, 1);
-        int prev = 0;
-        List<List<Integer>> res = new ArrayList<>();
-
-        for (int[] e : events) {
-            int x = e[0], h = e[1];
-            if (h < 0) { // entering
-                map.put(-h, map.getOrDefault(-h, 0) + 1);
-            } else { // leaving
-                map.put(h, map.get(h) - 1);
-                if (map.get(h) == 0) map.remove(h);
-            }
-            int cur = map.firstKey();
-            if (cur != prev) {
-                res.add(Arrays.asList(x, cur));
-                prev = cur;
-            }
-        }
-        return res;
+        return false;
     }
 }
