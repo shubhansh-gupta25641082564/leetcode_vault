c0c693f99e8e47535df6fd7778b99ec0e2795561
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-21 16:53:06 +0530

    File updated at 21/09/2025, 16:53:05

--- 206-ReverseLinkedList/206-ReverseLinkedList.java
+++ 206-ReverseLinkedList/206-ReverseLinkedList.java
@@ -1,36 +1,67 @@
-// Last updated: 21/09/2025, 16:49:51
-class WordDictionary {
-    private static class Node {
-        Node[] next = new Node[26];
-        boolean end;
+// Last updated: 21/09/2025, 16:53:05
+import java.util.*;
+
+class Solution {
+    private static class TrieNode {
+        TrieNode[] next = new TrieNode[26];
+        String word = null;
     }
-    private final Node root;
-    public WordDictionary() {
-        root = new Node();
-    }
-    public void addWord(String word) {
-        Node cur = root;
-        for (int i = 0; i < word.length(); ++i) {
-            int c = word.charAt(i) - 'a';
-            if (cur.next[c] == null) cur.next[c] = new Node();
-            cur = cur.next[c];
-        }
-        cur.end = true;
-    }
-    public boolean search(String word) {
-        return dfs(root, word, 0);
-    }
-    private boolean dfs(Node node, String word, int idx) {
-        if (node == null) return false;
-        if (idx == word.length()) return node.end;
-        char ch = word.charAt(idx);
-        if (ch == '.') {
-            for (int i = 0; i < 26; ++i) {
-                if (node.next[i] != null && dfs(node.next[i], word, idx + 1)) return true;
+
+    public List<String> findWords(char[][] board, String[] words) {
+        TrieNode root = buildTrie(words);
+        List<String> res = new ArrayList<>();
+        int m = board.length, n = board[0].length;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                dfs(board, i, j, root, res);
             }
-            return false;
-        } else {
-            return dfs(node.next[ch - 'a'], word, idx + 1);
         }
+        return res;
+    }
+
+    private TrieNode buildTrie(String[] words) {
+        TrieNode root = new TrieNode();
+        for (String w : words) {
+            TrieNode cur = root;
+            for (int i = 0; i < w.length(); ++i) {
+                int c = w.charAt(i) - 'a';
+                if (cur.next[c] == null) cur.next[c] = new TrieNode();
+                cur = cur.next[c];
+            }
+            cur.word = w;
+        }
+        return root;
+    }
+
+    private void dfs(char[][] board, int i, int j, TrieNode node, List<String> res) {
+        int m = board.length, n = board[0].length;
+        if (i < 0 || i >= m || j < 0 || j >= n) return;
+        char ch = board[i][j];
+        if (ch == '#') return;
+        TrieNode child = node.next[ch - 'a'];
+        if (child == null) return;
+
+        if (child.word != null) {
+            res.add(child.word);
+            child.word = null;
+        }
+
+        board[i][j] = '#';
+        dfs(board, i + 1, j, child, res);
+        dfs(board, i - 1, j, child, res);
+        dfs(board, i, j + 1, child, res);
+        dfs(board, i, j - 1, child, res);
+        board[i][j] = ch;
+
+        if (isEmpty(child)) {
+            node.next[ch - 'a'] = null;
+        }
+    }
+
+    private boolean isEmpty(TrieNode node) {
+        if (node == null) return true;
+        if (node.word != null) return false;
+        for (TrieNode t : node.next) if (t != null) return false;
+        return true;
     }
 }
