e545d7b0e96bae6e98fc1565c29bc834f1329df8
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-21 17:38:48 +0530

    File updated at 21/09/2025, 17:38:47

--- 206-ReverseLinkedList/206-ReverseLinkedList.java
+++ 206-ReverseLinkedList/206-ReverseLinkedList.java
@@ -1,36 +1,20 @@
-// Last updated: 21/09/2025, 17:37:00
-import java.util.*;
-
-class MyQueue {
-    private Stack<Integer> inStack;
-    private Stack<Integer> outStack;
-
-    public MyQueue() {
-        inStack = new Stack<>();
-        outStack = new Stack<>();
-    }
-
-    public void push(int x) {
-        inStack.push(x);
-    }
-
-    private void transfer() {
-        while (!inStack.isEmpty()) {
-            outStack.push(inStack.pop());
+// Last updated: 21/09/2025, 17:38:47
+class Solution {
+    public int countDigitOne(int n) {
+        long digit = 1, res = 0;
+        long high = n / 10, cur = n % 10, low = 0;
+        
+        while (high != 0 || cur != 0) {
+            if (cur == 0) res += high * digit;
+            else if (cur == 1) res += high * digit + low + 1;
+            else res += (high + 1) * digit;
+            
+            low += cur * digit;
+            cur = (int)(high % 10);
+            high /= 10;
+            digit *= 10;
         }
-    }
-
-    public int pop() {
-        if (outStack.isEmpty()) transfer();
-        return outStack.pop();
-    }
-
-    public int peek() {
-        if (outStack.isEmpty()) transfer();
-        return outStack.peek();
-    }
-
-    public boolean empty() {
-        return inStack.isEmpty() && outStack.isEmpty();
+        
+        return (int)res;
     }
 }
