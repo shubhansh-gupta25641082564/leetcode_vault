8f7f39c46799cd1a547f9a03e716528b4d983d7a
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-21 16:46:42 +0530

    File updated at 21/09/2025, 16:46:41

--- 206-ReverseLinkedList/206-ReverseLinkedList.java
+++ 206-ReverseLinkedList/206-ReverseLinkedList.java
@@ -1,26 +1,38 @@
-// Last updated: 21/09/2025, 16:45:34
-import java.util.*;
-
-class Solution {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<Integer>[] g = new List[numCourses];
-        for (int i = 0; i < numCourses; ++i) g[i] = new ArrayList<>();
-        int[] indeg = new int[numCourses];
-        for (int[] p : prerequisites) {
-            int a = p[0], b = p[1];
-            g[b].add(a);
-            indeg[a]++;
+// Last updated: 21/09/2025, 16:46:41
+class Trie {
+    private static class Node {
+        Node[] next = new Node[26];
+        boolean end;
+    }
+    private final Node root;
+    public Trie() {
+        root = new Node();
+    }
+    public void insert(String word) {
+        Node cur = root;
+        for (int i = 0; i < word.length(); ++i) {
+            int c = word.charAt(i) - 'a';
+            if (cur.next[c] == null) cur.next[c] = new Node();
+            cur = cur.next[c];
         }
-        Deque<Integer> q = new ArrayDeque<>();
-        for (int i = 0; i < numCourses; ++i) if (indeg[i] == 0) q.add(i);
-        int seen = 0;
-        while (!q.isEmpty()) {
-            int u = q.poll();
-            seen++;
-            for (int v : g[u]) {
-                if (--indeg[v] == 0) q.add(v);
-            }
+        cur.end = true;
+    }
+    public boolean search(String word) {
+        Node cur = root;
+        for (int i = 0; i < word.length(); ++i) {
+            int c = word.charAt(i) - 'a';
+            if (cur.next[c] == null) return false;
+            cur = cur.next[c];
         }
-        return seen == numCourses;
+        return cur.end;
+    }
+    public boolean startsWith(String prefix) {
+        Node cur = root;
+        for (int i = 0; i < prefix.length(); ++i) {
+            int c = prefix.charAt(i) - 'a';
+            if (cur.next[c] == null) return false;
+            cur = cur.next[c];
+        }
+        return true;
     }
 }
