890dbb04c7313c48e101ca1239c6e506511a4adf
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-21 17:00:01 +0530

    File updated at 21/09/2025, 17:00:00

--- 206-ReverseLinkedList/206-ReverseLinkedList.java
+++ 206-ReverseLinkedList/206-ReverseLinkedList.java
@@ -1,16 +1,18 @@
-// Last updated: 21/09/2025, 16:59:21
-import java.util.*;
-
+// Last updated: 21/09/2025, 17:00:00
 class Solution {
-    public boolean containsNearbyAlmostDuplicate(int[] nums, int indexDiff, int valueDiff) {
-        TreeSet<Long> set = new TreeSet<>();
-        for (int i = 0; i < nums.length; ++i) {
-            Long floor = set.floor((long) nums[i] + valueDiff);
-            Long ceil = set.ceiling((long) nums[i] - valueDiff);
-            if ((floor != null && floor >= nums[i]) || (ceil != null && ceil <= nums[i])) return true;
-            set.add((long) nums[i]);
-            if (set.size() > indexDiff) set.remove((long) nums[i - indexDiff]);
+    public int maximalSquare(char[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        int[][] dp = new int[m][n];
+        int maxSide = 0;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (matrix[i][j] == '1') {
+                    if (i == 0 || j == 0) dp[i][j] = 1;
+                    else dp[i][j] = 1 + Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1]));
+                    maxSide = Math.max(maxSide, dp[i][j]);
+                }
+            }
         }
-        return false;
+        return maxSide * maxSide;
     }
 }
