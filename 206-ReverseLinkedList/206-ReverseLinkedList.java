ebd0092bbf962c4526c5cb7aac608503ce416c47
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-21 17:04:22 +0530

    File updated at 21/09/2025, 17:04:21

--- 206-ReverseLinkedList/206-ReverseLinkedList.java
+++ 206-ReverseLinkedList/206-ReverseLinkedList.java
@@ -1,12 +1,36 @@
-// Last updated: 21/09/2025, 17:03:08
+// Last updated: 21/09/2025, 17:04:21
+import java.util.*;
+
 class Solution {
-    public int computeArea(int ax1, int ay1, int ax2, int ay2,
-                           int bx1, int by1, int bx2, int by2) {
-        int area1 = (ax2 - ax1) * (ay2 - ay1);
-        int area2 = (bx2 - bx1) * (by2 - by1);
-        int overlapWidth = Math.max(0, Math.min(ax2, bx2) - Math.max(ax1, bx1));
-        int overlapHeight = Math.max(0, Math.min(ay2, by2) - Math.max(ay1, by1));
-        int overlapArea = overlapWidth * overlapHeight;
-        return area1 + area2 - overlapArea;
+    public int calculate(String s) {
+        int n = s.length();
+        Deque<Integer> stack = new ArrayDeque<>();
+        int res = 0, sign = 1, i = 0;
+        while (i < n) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                int num = 0;
+                while (i < n && Character.isDigit(s.charAt(i))) {
+                    num = num * 10 + (s.charAt(i) - '0');
+                    i++;
+                }
+                res += sign * num;
+                continue;
+            } else if (c == '+') {
+                sign = 1;
+            } else if (c == '-') {
+                sign = -1;
+            } else if (c == '(') {
+                stack.push(res);
+                stack.push(sign);
+                res = 0;
+                sign = 1;
+            } else if (c == ')') {
+                res *= stack.pop(); // pop sign
+                res += stack.pop(); // pop previous res
+            }
+            i++;
+        }
+        return res;
     }
 }
