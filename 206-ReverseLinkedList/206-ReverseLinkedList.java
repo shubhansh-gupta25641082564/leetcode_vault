d3ae35270fe07b726038e634c639450b3d5006ed
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-21 16:49:51 +0530

    File updated at 21/09/2025, 16:49:51

--- 206-ReverseLinkedList/206-ReverseLinkedList.java
+++ 206-ReverseLinkedList/206-ReverseLinkedList.java
@@ -1,27 +1,36 @@
-// Last updated: 21/09/2025, 16:48:41
-import java.util.*;
-
-class Solution {
-    public int[] findOrder(int numCourses, int[][] prerequisites) {
-        List<Integer>[] g = new ArrayList[numCourses];
-        for (int i = 0; i < numCourses; ++i) g[i] = new ArrayList<>();
-        int[] indeg = new int[numCourses];
-        for (int[] p : prerequisites) {
-            int a = p[0], b = p[1];
-            g[b].add(a);
-            indeg[a]++;
+// Last updated: 21/09/2025, 16:49:51
+class WordDictionary {
+    private static class Node {
+        Node[] next = new Node[26];
+        boolean end;
+    }
+    private final Node root;
+    public WordDictionary() {
+        root = new Node();
+    }
+    public void addWord(String word) {
+        Node cur = root;
+        for (int i = 0; i < word.length(); ++i) {
+            int c = word.charAt(i) - 'a';
+            if (cur.next[c] == null) cur.next[c] = new Node();
+            cur = cur.next[c];
         }
-        Deque<Integer> q = new ArrayDeque<>();
-        for (int i = 0; i < numCourses; ++i) if (indeg[i] == 0) q.add(i);
-        int[] order = new int[numCourses];
-        int idx = 0;
-        while (!q.isEmpty()) {
-            int u = q.poll();
-            order[idx++] = u;
-            for (int v : g[u]) {
-                if (--indeg[v] == 0) q.add(v);
+        cur.end = true;
+    }
+    public boolean search(String word) {
+        return dfs(root, word, 0);
+    }
+    private boolean dfs(Node node, String word, int idx) {
+        if (node == null) return false;
+        if (idx == word.length()) return node.end;
+        char ch = word.charAt(idx);
+        if (ch == '.') {
+            for (int i = 0; i < 26; ++i) {
+                if (node.next[i] != null && dfs(node.next[i], word, idx + 1)) return true;
             }
+            return false;
+        } else {
+            return dfs(node.next[ch - 'a'], word, idx + 1);
         }
-        return idx == numCourses ? order : new int[0];
     }
 }
