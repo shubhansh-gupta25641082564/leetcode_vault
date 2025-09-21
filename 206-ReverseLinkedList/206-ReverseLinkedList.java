5fdf5a248c83a97e056c65c64d45a7665f9bd2ea
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-21 17:40:24 +0530

    File updated at 21/09/2025, 17:40:24

--- 206-ReverseLinkedList/206-ReverseLinkedList.java
+++ 206-ReverseLinkedList/206-ReverseLinkedList.java
@@ -1,38 +1,26 @@
-// Last updated: 21/09/2025, 17:39:27
+// Last updated: 21/09/2025, 17:40:24
 /**
- * Definition for singly-linked list.
- * public class ListNode {
+ * Definition for a binary tree node.
+ * public class TreeNode {
  *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-    public boolean isPalindrome(ListNode head) {
-        if (head == null || head.next == null) return true;
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return null;
         
-        ListNode slow = head, fast = head;
-        while (fast.next != null && fast.next.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
+        if (p.val < root.val && q.val < root.val) return lowestCommonAncestor(root.left, p, q);
+        if (p.val > root.val && q.val > root.val) return lowestCommonAncestor(root.right, p, q);
         
-        ListNode prev = null, curr = slow.next;
-        while (curr != null) {
-            ListNode temp = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = temp;
-        }
-        
-        ListNode first = head, second = prev;
-        while (second != null) {
-            if (first.val != second.val) return false;
-            first = first.next;
-            second = second.next;
-        }
-        return true;
+        return root;
     }
 }
