6dd277e1cf25af0bb22c34896f796b4fdcc687d5
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-21 16:47:24 +0530

    File updated at 21/09/2025, 16:47:23

--- 206-ReverseLinkedList/206-ReverseLinkedList.java
+++ 206-ReverseLinkedList/206-ReverseLinkedList.java
@@ -1,38 +1,17 @@
-// Last updated: 21/09/2025, 16:46:41
-class Trie {
-    private static class Node {
-        Node[] next = new Node[26];
-        boolean end;
-    }
-    private final Node root;
-    public Trie() {
-        root = new Node();
-    }
-    public void insert(String word) {
-        Node cur = root;
-        for (int i = 0; i < word.length(); ++i) {
-            int c = word.charAt(i) - 'a';
-            if (cur.next[c] == null) cur.next[c] = new Node();
-            cur = cur.next[c];
+// Last updated: 21/09/2025, 16:47:23
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int n = nums.length;
+        int left = 0;
+        int sum = 0;
+        int minLen = Integer.MAX_VALUE;
+        for (int right = 0; right < n; ++right) {
+            sum += nums[right];
+            while (sum >= target) {
+                minLen = Math.min(minLen, right - left + 1);
+                sum -= nums[left++];
+            }
         }
-        cur.end = true;
-    }
-    public boolean search(String word) {
-        Node cur = root;
-        for (int i = 0; i < word.length(); ++i) {
-            int c = word.charAt(i) - 'a';
-            if (cur.next[c] == null) return false;
-            cur = cur.next[c];
-        }
-        return cur.end;
-    }
-    public boolean startsWith(String prefix) {
-        Node cur = root;
-        for (int i = 0; i < prefix.length(); ++i) {
-            int c = prefix.charAt(i) - 'a';
-            if (cur.next[c] == null) return false;
-            cur = cur.next[c];
-        }
-        return true;
+        return minLen == Integer.MAX_VALUE ? 0 : minLen;
     }
 }
