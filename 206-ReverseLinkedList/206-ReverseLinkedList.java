7b4af0e65aeadd1940cfb72690547bb2675b2547
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-21 17:37:01 +0530

    File updated at 21/09/2025, 17:37:00

--- 206-ReverseLinkedList/206-ReverseLinkedList.java
+++ 206-ReverseLinkedList/206-ReverseLinkedList.java
@@ -1,6 +1,36 @@
-// Last updated: 21/09/2025, 17:17:41
-class Solution {
-    public boolean isPowerOfTwo(int n) {
-        return n > 0 && (n & (n - 1)) == 0;
+// Last updated: 21/09/2025, 17:37:00
+import java.util.*;
+
+class MyQueue {
+    private Stack<Integer> inStack;
+    private Stack<Integer> outStack;
+
+    public MyQueue() {
+        inStack = new Stack<>();
+        outStack = new Stack<>();
+    }
+
+    public void push(int x) {
+        inStack.push(x);
+    }
+
+    private void transfer() {
+        while (!inStack.isEmpty()) {
+            outStack.push(inStack.pop());
+        }
+    }
+
+    public int pop() {
+        if (outStack.isEmpty()) transfer();
+        return outStack.pop();
+    }
+
+    public int peek() {
+        if (outStack.isEmpty()) transfer();
+        return outStack.peek();
+    }
+
+    public boolean empty() {
+        return inStack.isEmpty() && outStack.isEmpty();
     }
 }
