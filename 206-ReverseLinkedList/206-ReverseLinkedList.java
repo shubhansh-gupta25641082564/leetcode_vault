6c5a7415ad9d0d7fa546e10a6c4aacf4f59a9f08
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-21 16:56:57 +0530

    File updated at 21/09/2025, 16:56:56

--- 206-ReverseLinkedList/206-ReverseLinkedList.java
+++ 206-ReverseLinkedList/206-ReverseLinkedList.java
@@ -1,23 +1,12 @@
-// Last updated: 21/09/2025, 16:56:27
+// Last updated: 21/09/2025, 16:56:56
 import java.util.*;
 
 class Solution {
-    public List<List<Integer>> combinationSum3(int k, int n) {
-        List<List<Integer>> res = new ArrayList<>();
-        backtrack(1, k, n, new ArrayList<>(), res);
-        return res;
-    }
-    private void backtrack(int start, int k, int target, List<Integer> cur, List<List<Integer>> res) {
-        if (target == 0 && k == 0) {
-            res.add(new ArrayList<>(cur));
-            return;
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (!set.add(num)) return true;
         }
-        if (k == 0) return;
-        for (int i = start; i <= 9; ++i) {
-            if (i > target) break;
-            cur.add(i);
-            backtrack(i + 1, k - 1, target - i, cur, res);
-            cur.remove(cur.size() - 1);
-        }
+        return false;
     }
 }
