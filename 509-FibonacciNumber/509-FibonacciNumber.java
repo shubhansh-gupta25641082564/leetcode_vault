85ff57594d9c732cbc7111c4961c7ae23ad9484a
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-22 12:07:01 +0530

    File updated at 22/09/2025, 12:06:59

--- 509-FibonacciNumber/509-FibonacciNumber.java
+++ 509-FibonacciNumber/509-FibonacciNumber.java
@@ -1,14 +1,27 @@
-// Last updated: 22/09/2025, 11:59:50
+// Last updated: 22/09/2025, 12:06:59
 class Solution {
-    public int fib(int n) {
-        if (n == 0) return 0;
-        if (n == 1) return 1;
-        int a = 0, b = 1;
-        for (int i = 2; i <= n; i++) {
-            int temp = a + b;
-            a = b;
-            b = temp;
+    public boolean detectCapitalUse(String word) {
+        int n = word.length();
+        if (n == 1) return true;
+        boolean firstCapital = Character.isUpperCase(word.charAt(0));
+        boolean secondCapital = Character.isUpperCase(word.charAt(1));
+
+        if (firstCapital && secondCapital) {
+            // All remaining must be capitals
+            for (int i = 2; i < n; i++) {
+                if (!Character.isUpperCase(word.charAt(i))) return false;
+            }
+        } else if (firstCapital) {
+            // All remaining must be lowercase
+            for (int i = 2; i < n; i++) {
+                if (!Character.isLowerCase(word.charAt(i))) return false;
+            }
+        } else {
+            // First is lowercase, all must be lowercase
+            for (int i = 1; i < n; i++) {
+                if (!Character.isLowerCase(word.charAt(i))) return false;
+            }
         }
-        return b;
+        return true;
     }
 }
