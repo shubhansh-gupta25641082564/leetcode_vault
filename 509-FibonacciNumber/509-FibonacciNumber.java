513e290587feb8eed32ac20cbb1fe6d84c86472c
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-22 12:09:07 +0530

    File updated at 22/09/2025, 12:09:06

--- 509-FibonacciNumber/509-FibonacciNumber.java
+++ 509-FibonacciNumber/509-FibonacciNumber.java
@@ -1,17 +1,32 @@
-// Last updated: 22/09/2025, 12:07:51
+// Last updated: 22/09/2025, 12:09:06
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public String reverseStr(String s, int k) {
-        char[] arr = s.toCharArray();
-        for (int i = 0; i < arr.length; i += 2 * k) {
-            int left = i, right = Math.min(i + k - 1, arr.length - 1);
-            while (left < right) {
-                char temp = arr[left];
-                arr[left] = arr[right];
-                arr[right] = temp;
-                left++;
-                right--;
-            }
-        }
-        return new String(arr);
+    private int maxDiameter = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        depth(root);
+        return maxDiameter;
+    }
+
+    private int depth(TreeNode node) {
+        if (node == null) return 0;
+        int left = depth(node.left);
+        int right = depth(node.right);
+        maxDiameter = Math.max(maxDiameter, left + right);
+        return Math.max(left, right) + 1;
     }
 }
