8c716c971c51aa3ccb0ef1f1644be9303a8874f8
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-09-23 21:43:01 +0530

    File updated at 23/09/2025, 21:42:58

--- 165-CompareVersionNumbers/165-CompareVersionNumbers.java
+++ 165-CompareVersionNumbers/165-CompareVersionNumbers.java
@@ -1,14 +1,35 @@
-// Last updated: 20/09/2025, 00:31:27
+// Last updated: 23/09/2025, 21:42:58
 class Solution {
     public int compareVersion(String version1, String version2) {
-        String[] a = version1.split("\\.");
-        String[] b = version2.split("\\.");
-        int n = Math.max(a.length, b.length);
-        for (int i = 0; i < n; i++) {
-            int x = i < a.length ? Integer.parseInt(a[i]) : 0;
-            int y = i < b.length ? Integer.parseInt(b[i]) : 0;
-            if (x < y) return -1;
-            if (x > y) return 1;
+        String[] v1 = version1.split("\\.");
+        String[] v2 = version2.split("\\.");
+        int len = Math.max(v1.length, v2.length), x = 0, y = 0;
+        for (int i = 0; i < len; i ++)
+        {
+            if (i < v1.length)
+            {
+                x = Integer.parseInt(v1[i]);
+            }
+            else
+            {
+                x = 0;
+            }
+            if (i < v2.length)
+            {
+                y = Integer.parseInt(v2[i]);
+            }
+            else
+            {
+                y = 0;
+            }
+            if (x < y)
+            {
+                return -1;
+            }
+            else if (x > y)
+            {
+                return 1;
+            }
         }
         return 0;
     }
