29ffa5445f4bbf7ee8685eec3caedd0aa112f8ce
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-10-01 22:23:39 +0530

    Create 969-NumberOfRecentCalls

---
+++ 969-NumberOfRecentCalls/969-NumberOfRecentCalls.java
@@ -0,0 +1,25 @@
+// Last updated: 01/10/2025, 22:23:39
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class RecentCounter {
+    private Deque<Integer> queue;
+
+    public RecentCounter() {
+        queue = new ArrayDeque<>();
+    }
+
+    public int ping(int t) {
+        queue.offer(t);
+        while (queue.peek() < t - 3000) {
+            queue.poll();
+        }
+        return queue.size();
+    }
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */
