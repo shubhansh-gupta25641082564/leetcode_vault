ca2267d2808d7969eae575658d1575d1e1119061
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2025-11-02 23:39:58 +0530

    Create 2169-SimpleBankSystem

---
+++ 2169-SimpleBankSystem/2169-SimpleBankSystem.java
@@ -0,0 +1,35 @@
+// Last updated: 02/11/2025, 23:39:58
+class Bank {
+    private final long[] bal;
+
+    public Bank(long[] balance) {
+        this.bal = balance;
+    }
+
+    private boolean valid(int acc) {
+        return acc >= 1 && acc <= bal.length;
+    }
+
+    public boolean transfer(int account1, int account2, long money) {
+        if (!valid(account1) || !valid(account2)) return false;
+        int a = account1 - 1, b = account2 - 1;
+        if (bal[a] < money) return false;
+        bal[a] -= money;
+        bal[b] += money;
+        return true;
+    }
+
+    public boolean deposit(int account, long money) {
+        if (!valid(account)) return false;
+        bal[account - 1] += money;
+        return true;
+    }
+
+    public boolean withdraw(int account, long money) {
+        if (!valid(account)) return false;
+        int a = account - 1;
+        if (bal[a] < money) return false;
+        bal[a] -= money;
+        return true;
+    }
+}
