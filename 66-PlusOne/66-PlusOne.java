9098c1125221be6b6fe4bde73a478b982748fc9c
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2026-01-01 18:32:43 +0530

    File updated at 01/01/2026, 18:32:43

--- 66-PlusOne/66-PlusOne.java
+++ 66-PlusOne/66-PlusOne.java
@@ -1,16 +1,16 @@
-// Last updated: 10/09/2025, 15:07:58
-class Solution {
-    public int[] plusOne(int[] digits) {
-        for (int i = digits.length - 1; i >= 0; i--) {
-            if (digits[i] < 9) {
-                digits[i]++;
-                return digits;
-            }
-            digits[i] = 0;
-        }
-
-        int[] result = new int[digits.length + 1];
-        result[0] = 1;
-        return result;
-    }
-}
+// Last updated: 01/01/2026, 18:32:43
+1class Solution {
+2    public int[] plusOne(int[] digits) {
+3        for (int i = digits.length - 1; i >= 0; i--) {
+4            if (digits[i] < 9) {
+5                digits[i]++;
+6                return digits;
+7            }
+8            digits[i] = 0;
+9        }
+10
+11        int[] result = new int[digits.length + 1];
+12        result[0] = 1;
+13        return result;
+14    }
+15}
