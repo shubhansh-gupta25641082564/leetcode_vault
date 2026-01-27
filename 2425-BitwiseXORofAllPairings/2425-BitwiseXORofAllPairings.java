11971ff1647c99218b1c7481e862ac14da371cd7
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2026-01-27 16:21:45 +0530

    File updated at 27/01/2026, 16:21:47

--- 2425-BitwiseXORofAllPairings/2425-BitwiseXORofAllPairings.java
+++ 2425-BitwiseXORofAllPairings/2425-BitwiseXORofAllPairings.java
@@ -1,15 +1,32 @@
-// Last updated: 27/01/2026, 16:21:25
+// Last updated: 27/01/2026, 16:21:47
 1class Solution {
-2    public int getXORSum(int[] arr1, int[] arr2) {
-3        int xor1 = Bitwise_XOR(arr1);
-4        int xor2 = Bitwise_XOR(arr2);
-5        return (xor1 & xor2);
-6    }
-7    public static int Bitwise_XOR(int[] arr) {
-8        int xor = 0;
-9        for(int v : arr) {
-10            xor ^= v;
-11        }
-12        return xor;
-13    }
-14}
+2    public int minimizeXor(int num1, int num2) {
+3        int set_bit = 0;
+4        while(num2 > 0) {
+5            num2 = (num2 & (num2 - 1));
+6            set_bit ++;
+7        }
+8        int x = 0;
+9        for(int i = 30; i >= 0; i --) {
+10            int mask = (1 << i);
+11            if((num1 & mask) != 0) {
+12                x |= mask; // x=x | mask
+13                set_bit --;
+14                if(set_bit == 0) {
+15                    return x;
+16                }
+17            }
+18        }
+19        for(int i = 0; i <= 30; i ++) {
+20            int mask = (1 << i);
+21            if((num1 & mask) == 0) {
+22                x |= mask;
+23                set_bit --;
+24                if(set_bit == 0) {
+25                    return x;
+26                }
+27            }
+28        }
+29        return x;
+30    }
+31}
