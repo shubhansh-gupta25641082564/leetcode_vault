c9a8b61a7b360cf1bf732779425fa7df9b966d97
Shubhansh Gupta <160236613+shubhansh-gupta25641082564@users.noreply.github.com>
2026-02-13 21:29:04 +0530

    File updated at 13/02/2026, 21:29:05

--- 2874-MaximumValueofanOrderedTripletII/2874-MaximumValueofanOrderedTripletII.java
+++ 2874-MaximumValueofanOrderedTripletII/2874-MaximumValueofanOrderedTripletII.java
@@ -1,20 +1,19 @@
-// Last updated: 13/02/2026, 21:26:45
+// Last updated: 13/02/2026, 21:29:05
 1class Solution {
-2    public long maximumTripletValue(int[] nums) {
-3        long ans = 0;
-4        long maxNum = 0;   // best nums[i] so far
-5        long maxDiff = 0;  // best (nums[i] - nums[j]) so far for some i < j
-6
-7        for (int x : nums) {
-8            // x acts as nums[k]
-9            ans = Math.max(ans, maxDiff * (long) x);
-10
-11            // x acts as nums[j]
-12            maxDiff = Math.max(maxDiff, maxNum - (long) x);
-13
-14            // x acts as nums[i]
-15            maxNum = Math.max(maxNum, (long) x);
-16        }
-17        return ans;
-18    }
-19}
+2    public int maxFrequency(int[] nums, int k) {
+3        int max = 0, ck = 0;
+4        int[] prefix = new int[51];
+5
+6        for(int i = 0; i < nums.length; i++) {
+7            if(nums[i] == k) {
+8                ck++;
+9                prefix[k] = max + 1;
+10            } else {
+11                prefix[nums[i]] = Math.max(prefix[nums[i]], ck) + 1;
+12            }
+13            max = Math.max(max, prefix[nums[i]]);
+14        }
+15
+16        return max;
+17    }
+18}
